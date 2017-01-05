91c22517acd2b81723806cf76cd9e74c3b2e8985
naladehoff62 <naladehoff62@gmail.com>
2017-01-05 14:13:42 -0600

    Name.java

---
+++ Name.java
@@ -0,0 +1,49 @@
+package bean;
+
+public class Name {
+	private String fname;
+	private String lname;
+	private String sid;
+	private String cname;
+	private String cid;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getSID() {
+		return sid;
+	}
+
+	public void setSID(String sid) {
+		this.sid = sid;
+	}
+	
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	public String getCID() {
+		return cid;
+	}
+
+	public void setCID(String cid) {
+		this.cid = cid;
+	}
+}
